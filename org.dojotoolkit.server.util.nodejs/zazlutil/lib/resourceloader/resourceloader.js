8c95de07a447c620b9ad1ab3d4313041b335f9c0
Richard Backhouse <richardabackhouse@gmail.com>
2011-03-29 16:55:51 -0400

    Support Uglify-JS compression via an AMD loader

--- org.dojotoolkit.server.util.nodejs/zazlutil/lib/resourceloader/resourceloader.js
+++ org.dojotoolkit.server.util.nodejs/zazlutil/lib/resourceloader/resourceloader.js
@@ -9,6 +9,7 @@ var jsp = require("uglify-js").parser;
 var pro = require("uglify-js").uglify;
 
 var providerPaths = [];
+var ignoreList = [/dojo\/_base\/html.js/];
 
 exports.addProvider = function(providerPath) {
 	//console.log("provider path ["+providerPath+"] added");
@@ -18,15 +19,21 @@ exports.addProvider = function(providerPath) {
 exports.addProvider(path.dirname(module.filename));
 
 exports.readText = function(filePath, compress) {
+	if (compress === undefined) {
+		compress = true;
+	}
     var contents = null;
     for (var i = 0; i < providerPaths.length; i++) {
     	contents = readTextFile(filePath, providerPaths[i]);
         if (contents !== null) {
-        	if (compress) {
+        	if (doCompress(filePath, compress)) {
         		var ast = jsp.parse(contents);
         		ast = pro.ast_mangle(ast);
         		ast = pro.ast_squeeze(ast, {make_seqs: false});
         		contents = pro.gen_code(ast);
+                if (contents.charAt(contents.length-1) === ')') {
+                	contents += ";";
+                }
         	}
         	break;
         }
@@ -34,6 +41,22 @@ exports.readText = function(filePath, compress) {
     //console.log("readText : ["+filePath+"] "+ ((contents === null) ? "false" : "true"));
     return contents;
 };
+
+doCompress = function(filePath, compressFlag) {
+	var compress = false;
+	var path = String(filePath);
+	if (compressFlag && path.match(".js$")) {
+		var ignore = false;
+		for (var i = 0; i < ignoreList.length; i++) {
+			if (path.match(ignoreList[i])) {
+				ignore = true;
+				break;
+			}
+		}
+		compress = !ignore;
+	}
+	return compress;
+}
 
 readTextFile = function(filePath, root) {
     filePath = path.join(root, String(filePath));
