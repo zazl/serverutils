867ecfa784baf438db1a7081df48b31b2749e9f1
Richard Backhouse <richardabackhouse@gmail.com>
2012-01-27 15:19:41 -0500

    Add a comment indicating the code donation from Pat Mueller

--- org.dojotoolkit.json/src/org/dojotoolkit/json/internal/Token.java
+++ org.dojotoolkit.json/src/org/dojotoolkit/json/internal/Token.java
@@ -2,6 +2,8 @@
     Copyright (c) 2004-2010, The Dojo Foundation All Rights Reserved.
     Available via Academic Free License >= 2.1 OR the modified BSD license.
     see: http://dojotoolkit.org/license for details
+
+    This code has been generously donated by Pat Mueller (http://muellerware.org/)
 */
 package org.dojotoolkit.json.internal;
 
